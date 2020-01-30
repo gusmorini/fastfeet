260e7f5b6f50451a17ac16a82360c4c176ddf25b
Gustavo Morini <gustavommh@gmail.com>
2020-01-30 19:32:27 -0300

    config database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mariadb',
+  host: 'localhost',
+  username: 'root',
+  password: 'admin',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
