368548927c83f807885ca0f6fe8b755510d1a5bd
Gustavo Morini <gustavommh@gmail.com>
2020-01-30 14:47:30 -0300

    config eslint + prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import express from 'express';
+import express from "express";
 
-import routes from './routes';
+import routes from "./routes";
 
 class App {
-
   constructor() {
     this.server = express();
     this.middlewares();
@@ -17,7 +16,6 @@ class App {
   routes() {
     this.server.use(routes);
   }
-
 }
 
 export default new App().server;
