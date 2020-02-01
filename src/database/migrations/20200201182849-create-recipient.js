4a8311923c2f4e397ce54ef732a9759f068ac96d
Gustavo Morini <gustavommh@gmail.com>
2020-02-01 15:40:23 -0300

    migrate recipient

---
+++ src/database/migrations/20200201182849-create-recipient.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipient', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      complement: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      zip_code: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipient');
+  },
+};
+
+// nome do destinatário e campos
+// de endereço: rua, número, complemento,
+// estado, cidade e CEP.
+
+// street, number, complement, state, city and zip code
