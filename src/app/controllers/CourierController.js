83ee6926b23cfd87d9de27527cebcc6cee2ae7ec
Gustavo Morini <gustavommh@gmail.com>
2020-02-08 15:29:44 -0300

    Courier already registred

--- src/app/controllers/CourierController.js
+++ src/app/controllers/CourierController.js
@@ -44,6 +44,25 @@ class CourierController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      avatar_id: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Invalid data' });
+    }
+
+    if (req.body.email) {
+      const exists = await Courier.findOne({
+        where: { email: req.body.email },
+      });
+      if (exists) {
+        return res.status(401).json({ error: 'Courier already registred' });
+      }
+    }
+
     res.json({ ok: 'update' });
   }
 }
